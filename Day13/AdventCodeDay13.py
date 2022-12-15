7b4eea6a4b37b51f27b3679c48cfb0e2181af5f3
Kyle McBrady <kyle.mcbrady@gmail.com>
2022-12-14 19:52:39 -0600

    Day 13 Parsing Done

---
+++ Day13/AdventCodeDay13.py
@@ -0,0 +1,49 @@
+def parse(string):
+    listOfLists = []
+    justAList = []
+    charHolder = 'New'
+
+
+
+    for char in string:
+
+        #store current list, go back to previous list, and addin current list
+        if charHolder != 'New' and not char.isdigit():
+            justAList = listOfLists.pop()
+            justAList.append(int(charHolder))
+            charHolder = 'New'
+
+        #append current working list and creat a new on
+        if char == '[':
+            listOfLists.append(justAList)
+            justAList = []
+
+        #store current list, go back to previous list, and addin current list
+        elif char == ']':
+            tempList = justAList
+            justAList = listOfLists.pop()
+            justAList.append(tempList)
+
+        elif char.isdigit():
+            if charHolder == 'New':
+                charHolder = ''
+                listOfLists.append(justAList)
+                justAList = []
+            charHolder = charHolder + char
+    return justAList[0] #pull off the extra list
+
+
+with open('input.txt') as f:
+    data = f.readlines()
+
+leftArray = []
+rightArray = []
+x = 0
+while x < len(data):
+    leftArray.append(parse(data[x]))
+    rightArray.append(parse(data[x+1]))
+    x += 3
+
+print('Parsing Done')
+
+
