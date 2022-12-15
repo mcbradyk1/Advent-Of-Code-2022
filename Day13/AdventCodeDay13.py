4e1d8dc61253815ed1808d29d01c506fdd2b494f
Kyle McBrady <kyle.mcbrady@gmail.com>
2022-12-15 07:45:57 -0600

    Day 13 part 1 done

--- Day13/AdventCodeDay13.py
+++ Day13/AdventCodeDay13.py
@@ -34,6 +34,37 @@ def parse(string):
 """
 
 
+def compare(left,right):
+    if isinstance(left, int) and isinstance(right, int):
+        if left < right: return 1
+        if left > right: return -1
+        else: return 0
+
+    # make unmatching data sets lists
+    if isinstance(left, list) and isinstance(right,int): right = [right]
+    elif isinstance(left, int) and isinstance(right, list): left = [left]
+
+    # should be both lists here
+    index = 0
+
+    #while both lists have values, compare again
+    while index < min(len(left),len(right)):
+        
+        result = compare(left[index],right[index])
+
+        if result == 0:
+            index += 1
+        else:
+            return result
+
+    #ended without size difference check list length
+    if len(left) < len(right): return 1
+    if len(left) > len(right): return -1
+    else: return 0
+    
+
+
+
 with open('input.txt') as f:
     data = f.readlines()
 
@@ -47,6 +78,14 @@ while x < len(data):
     x += 3
 
 print('Parsing Done')
+result = 0
+for x in range(len(leftArray)):
+    if compare(leftArray[x],rightArray[x]) == 1:
+        result += x + 1
+
+
+print(result)
+
 
 
 
